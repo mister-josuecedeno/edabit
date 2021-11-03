f6003857217d3472bd75c7b74425f7e68d22e753
mister-josuecedeno <josuecedeno@yahoo.com>
2021-11-02 20:29:29 -0500

    sleepZones (using Moment.js)

--- 03 - Medium/sleepZones.js
+++ 03 - Medium/sleepZones.js
@@ -1,7 +1,18 @@
 // calculate six sleep zones based on time provided
 
 const sleepZones = () => {
-  // var newDateObj = moment(oldDateObj).add(30, 'm').toDate();
+
+  let now = moment();
+  let zones = [];
+  let zone;
+
+  for (let i = 1; i < 7; i++) {
+    zone = moment(now).add((90 * i + 15), 'm');
+    zones.push(zone);
+  }
+
+  console.log(now.format("LT"));
+  console.log(zones[5].format("LT"));
 }
 
 // Tests
