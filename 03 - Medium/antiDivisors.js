6e7c44fd7209b8abd52c8a9fb6c826c080203bd8
mister-josuecedeno <josuecedeno@yahoo.com>
2022-01-27 07:45:25 -0600

    antiDivisor

--- 03 - Medium/antiDivisors.js
+++ 03 - Medium/antiDivisors.js
@@ -1,17 +1,14 @@
-// In progress
 const antiDivisors = (n) => {
   const isAntiDivisor = (num) => {
     let isOdd = num % 2 !== 0;
     if (isOdd) return (n * 2 - 1) % num === 0 || (n * 2 + 1) % num === 0;
 
     let isEven = num % 2 === 0;
-    if (isEven) return num % (n * 2) === 0;
-
-    return (isOdd && isOddDivisor) || (isEven && isEvenDivisor);
+    if (isEven) return (n * 2) % num === 0;
   };
 
   let checked = [...Array(Math.abs(n)).keys()].filter((d) => {
-    return d > 1 && d < n;
+    return d > 1 && d < n && n % d !== 0;
   });
 
   return checked.filter((c) => isAntiDivisor(c));
@@ -21,15 +18,15 @@ const antiDivisors = (n) => {
 
 console.log(antiDivisors(10), [3, 4, 7], 'Example #1');
 console.log(antiDivisors(12), [5, 8], 'Example #2');
-// console.log(antiDivisors(20), [3, 8, 13], 'Example #3');
-// console.log(antiDivisors(1), []);
-// console.log(antiDivisors(31), [2, 3, 7, 9, 21]);
-// console.log(antiDivisors(55), [2, 3, 10, 22, 37]);
-// console.log(antiDivisors(0), []);
-// console.log(antiDivisors(105), [2, 6, 10, 11, 14, 19, 30, 42, 70]);
-// console.log(antiDivisors(666), [4, 11, 12, 31, 36, 43, 121, 148, 444]);
-// console.log(
-//   antiDivisors(9999),
-//   [2, 6, 7, 18, 22, 66, 198, 202, 606, 1818, 2222, 2857, 6666]
-// );
-// console.log(antiDivisors(-33), []);
+console.log(antiDivisors(20), [3, 8, 13], 'Example #3');
+console.log(antiDivisors(1), []);
+console.log(antiDivisors(31), [2, 3, 7, 9, 21]);
+console.log(antiDivisors(55), [2, 3, 10, 22, 37]);
+console.log(antiDivisors(0), []);
+console.log(antiDivisors(105), [2, 6, 10, 11, 14, 19, 30, 42, 70]);
+console.log(antiDivisors(666), [4, 11, 12, 31, 36, 43, 121, 148, 444]);
+console.log(
+  antiDivisors(9999),
+  [2, 6, 7, 18, 22, 66, 198, 202, 606, 1818, 2222, 2857, 6666]
+);
+console.log(antiDivisors(-33), []);
