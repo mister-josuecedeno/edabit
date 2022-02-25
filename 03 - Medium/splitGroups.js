e31c78fa791120114352b52625c7de2bbf654d5d
mister-josuecedeno <josuecedeno@yahoo.com>
2022-02-25 07:55:47 -0600

    splitGroups

--- 03 - Medium/splitGroups.js
+++ 03 - Medium/splitGroups.js
@@ -1,7 +1,19 @@
-// set-up
+// Regex solution >>> const splitGroups = str => str.match(/(.)\1*/g);
+
 const splitGroups = (str) => {
-  const regex = /.+/g;
-  return str.match(regex);
+  const result = [];
+  let letters = str[0];
+
+  for (let i = 1; i < str.length + 1; i++) {
+    if (str[i] !== str[i - 1]) {
+      result.push(letters);
+      letters = str[i];
+    } else {
+      letters += str[i];
+    }
+  }
+
+  return result;
 };
 
 // Tests
