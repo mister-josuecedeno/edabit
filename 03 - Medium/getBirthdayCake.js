c483bc7a168aba6bbb887b10df034485c42c0d55
mister-josuecedeno <josuecedeno@yahoo.com>
2022-03-28 07:29:02 -0500

    getBirthdayCake

--- 03 - Medium/getBirthdayCake.js
+++ 03 - Medium/getBirthdayCake.js
@@ -1,5 +1,13 @@
 const getBirthdayCake = (name, age) => {
-  return;
+  const frosting = age % 2 === 0 ? '#' : '*';
+  const message = `${age} Happy Birthday ${name}! ${age}`;
+  const layer = frosting.repeat(message.length + 4);
+  return [layer, `${frosting} ${message} ${frosting}`, layer];
 };
 
 // Tests
+console.log(getBirthdayCake('Jack', 10), [
+  '##############################',
+  '# 10 Happy Birthday Jack! 10 #',
+  '##############################',
+]);
