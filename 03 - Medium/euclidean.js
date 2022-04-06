b312bab0703a32f6be336339a85b3868d17a4976
mister-josuecedeno <josuecedeno@yahoo.com>
2022-04-05 20:43:20 -0500

    euclidean

--- 03 - Medium/euclidean.js
+++ 03 - Medium/euclidean.js
@@ -1,12 +1,11 @@
-const euclidean = (a, b) => {
-  return;
-};
+const euclidean = (a, b) =>
+  b > a ? euclidean(b, a) : a % b ? euclidean(b, a % b) : b;
 
 // Tests
 
-Test.assertEquals(euclidean(4, 2), 2);
-Test.assertEquals(euclidean(25, 5), 5);
-Test.assertEquals(euclidean(280, 64), 8);
-Test.assertEquals(euclidean(3, 6), 3);
-Test.assertEquals(euclidean(64, 52), 4);
-Test.assertEquals(euclidean(49, 14), 7);
+console.log(euclidean(4, 2), 2);
+console.log(euclidean(25, 5), 5);
+console.log(euclidean(280, 64), 8);
+console.log(euclidean(3, 6), 3);
+console.log(euclidean(64, 52), 4);
+console.log(euclidean(49, 14), 7);
