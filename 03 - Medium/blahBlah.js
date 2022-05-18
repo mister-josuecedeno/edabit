e87d0941a989b12ddf20293e614fed6cf1908931
mister-josuecedeno <josuecedeno@yahoo.com>
2022-05-18 07:58:36 -0500

    blahBlah (set-up)

---
+++ 03 - Medium/blahBlah.js
@@ -0,0 +1,29 @@
+// set-up
+const blahBlah = (str, n) => {
+  return;
+};
+
+// Tests
+
+console.log(
+  blahBlah(
+    'A function is a block of code which only runs when it is called',
+    5
+  ),
+  'A function is a block of code which only blah blah blah blah blah...'
+);
+console.log(
+  blahBlah('one two three four five', 2),
+  'one two three blah blah...'
+);
+console.log(
+  blahBlah('Sphinx of black quartz judge my vow', 10),
+  'blah blah blah blah blah blah blah...'
+);
+console.log(
+  blahBlah(
+    'The quadratic formula is negative b plus or minus the square root of b squared subtract four ac all over two a',
+    5
+  ),
+  'The quadratic formula is negative b plus or minus the square root of b squared subtract four blah blah blah blah blah...'
+);
