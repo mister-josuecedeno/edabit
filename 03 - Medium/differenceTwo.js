b47ebfa4f0e710733312a90cfff8cb9dce189f08
mister-josuecedeno <josuecedeno@yahoo.com>
2022-06-03 07:46:47 -0500

    differenceTwo

--- 03 - Medium/differenceTwo.js
+++ 03 - Medium/differenceTwo.js
@@ -1,15 +1,15 @@
-// Set up
 const differenceTwo = (nums) => {
-  const sorted = nums.sort((a, b) => b - a);
+  const sorted = [...nums.sort((a, b) => b - a)];
   const result = [];
 
   for (let i = 0; i < nums.length; i++) {
     let max = sorted.shift();
     let twos = sorted.filter((n) => max - n === 2);
-    result.push(twos);
+    twos.push(max);
+    if (twos.length === 2) result.push(twos);
   }
 
-  return result;
+  return result.sort((a, b) => a[0] - b[0]);
 };
 
 // Tests
--- README.md
+++ README.md
@@ -495,6 +495,7 @@ Additional code examples can be found under my profile in Edabit. This migration
 2022-05-31 - swapcards.js\
 2022-06-01 - validateSpelling.js\
 2022-06-02 - differenceTwo.js\
+2022-06-03 - differenceTwo.js\
 
 ## Hard
 
