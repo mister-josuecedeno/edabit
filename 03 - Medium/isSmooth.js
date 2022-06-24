36498616e6bb45bc2fef4d8f737be28eeb38f10d
mister-josuecedeno <josuecedeno@yahoo.com>
2022-06-24 08:03:09 -0500

    isSmooth

---
+++ 03 - Medium/isSmooth.js
@@ -0,0 +1,19 @@
+// set-up
+const isSmooth = (sentence) => {
+  return;
+};
+
+// Tests
+
+console.log(
+  isSmooth('Marta appreciated deep perpendicular right trapezoids'),
+  true
+);
+console.log(isSmooth('Someone is outside the doorway'), false);
+console.log(isSmooth('She eats super righteously'), true);
+console.log(isSmooth('Ben naps so often'), true);
+console.log(isSmooth('Cute triangles are cute'), false);
+console.log(isSmooth('Mad dislikes sharp pointy yoyos'), true);
+console.log(isSmooth('Rita asks Sam mean numbered dilemmas'), true);
+console.log(isSmooth('Marigold daffodils streaming happily.'), false);
+console.log(isSmooth('Simply wonderful laughing.'), false);
