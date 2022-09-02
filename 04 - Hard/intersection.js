539ddc4cd81511bcaa735fba8979623ca756e8f0
mister-josuecedeno <josuecedeno@yahoo.com>
2022-09-02 07:55:02 -0500

    add logic matching arrays (still need objects)

--- 04 - Hard/intersection.js
+++ 04 - Hard/intersection.js
@@ -1,15 +1,33 @@
 // https://edabit.com/challenge/96PKn2siDra5D7oPD
 
 // set-up
+
+const getMatches = (obj1, obj2) => {
+  // this works for arrays but not objects
+
+  const matches = obj1.filter((e) => {
+    return obj2.includes(e);
+  });
+
+  return matches;
+};
+
 const intersection = (...a) => {
   const [first, ...others] = a;
+  const matches = new Set();
 
-  // Add psuedocode
+  for (let i = 0; i < others.length; i++) {
+    matches.add(getMatches(first, others[i]).join(', '));
+  }
+
+  return [...matches];
 };
 
 // Tests
 
+/*
 console.log(intersection([1, 2, 3], [3, 4, 5], [3, 6, 7]), [3]);
+
 console.log(
   intersection(
     ['cat', 'dog', 'mouse'],
@@ -18,6 +36,9 @@ console.log(
   ),
   ['cat', 'dog']
 );
+
+*/
+
 console.log(
   intersection(
     [
