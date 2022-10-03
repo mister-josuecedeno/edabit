00c5c505929e75bd73f66bf1105ab97d1540cd7b
mister-josuecedeno <josuecedeno@yahoo.com>
2022-10-03 07:54:13 -0500

    balanced

--- 04 - Hard/balanced.js
+++ 04 - Hard/balanced.js
@@ -1,5 +1,33 @@
 // https://edabit.com/challenge/NcQg8tCkMecNpjwXc
 
 const balanced = (arr) => {
-  return;
+  const mid = arr.length / 2;
+
+  const first = arr.slice(0, mid);
+  const second = arr.slice(-mid);
+
+  const firstSum = first.reduce((acc, cv) => acc + cv, 0);
+  const secondSum = second.reduce((acc, cv) => acc + cv, 0);
+
+  if (firstSum === secondSum) {
+    return arr;
+  } else if (firstSum > secondSum) {
+    return [...first, ...first];
+  } else {
+    return [...second, ...second];
+  }
 };
+
+// Tests
+
+console.log(balanced([1, 2, 4, 6, 3, 1]), [6, 3, 1, 6, 3, 1]);
+console.log(
+  balanced([88, 3, 27, 5, 9, 0, 13, 10]),
+  [88, 3, 27, 5, 88, 3, 27, 5]
+);
+console.log(
+  balanced([7, 5, 2, 6, 1, 0, 1, 5, 2, 7, 0, 6]),
+  [7, 5, 2, 6, 1, 0, 1, 5, 2, 7, 0, 6]
+);
+console.log(balanced([0, 1, 1, 1]), [1, 1, 1, 1]);
+console.log(balanced([100, 55]), [100, 100]);
--- README.md
+++ README.md
@@ -673,7 +673,8 @@ Additional code examples can be found under my profile in Edabit. This migration
 2022-09-29 - threeDaysAgo.js\
 2022-09-30 - pairs.js\
 2022-10-01 - pairs.js\
-2022-01-02 - balanced,js\
+2022-01-02 - balanced.js\
+2022-01-03 - balanced.js\
 
 ## Very Hard
 
