732da9d3bd6525088472aa989b1d5e792078dce0
mister-josuecedeno <josuecedeno@yahoo.com>
2022-10-29 10:58:17 -0400

    snakefill

--- 04 - Hard/snakefill.js
+++ 04 - Hard/snakefill.js
@@ -1,3 +1,21 @@
-const snakefill = (n) => {
-  return;
-};
+// https://edabit.com/challenge/gQuex4Ldsohmzscb3
+
+// NOt my solution
+function snakefill(x) {
+  const max = x * x;
+  let sum = 1;
+  let count = 0;
+  while (sum <= max) {
+    sum += sum;
+    if (sum <= max) count++;
+  }
+  return count;
+}
+
+// Tests
+console.log(snakefill(8), 6);
+console.log(snakefill(18), 8);
+console.log(snakefill(555), 18);
+console.log(snakefill(2), 2);
+console.log(snakefill(1), 0);
+console.log(snakefill(900), 19);
