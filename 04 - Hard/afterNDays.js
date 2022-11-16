01d8eea25e6b99fc6acb183435449f2fc4473862
mister-josuecedeno <josuecedeno@yahoo.com>
2022-11-16 08:02:09 -0600

    refactor

--- 04 - Hard/afterNDays.js
+++ 04 - Hard/afterNDays.js
@@ -1,5 +1,18 @@
 // https://edabit.com/challenge/fLDHdMhmaNEd528wB
 
+// refactor
+const getIndex = (max = 7, start = 0, add = 0) => {
+  let newIndex = start;
+
+  for (let i = start; i <= add; i++) {
+    newIndex = newIndex + 1 >= max ? 0 : newIndex++;
+    console.log(newIndex);
+  }
+  return newIndex;
+};
+
+console.log(getIndex(7, 4, 1));
+
 const daysOfTheWeek = [
   'Sunday',
   'Monday',
@@ -10,11 +23,10 @@ const daysOfTheWeek = [
   'Saturday',
 ];
 
-// refactor - what to do when the number is greater than the aray length
 const getDayOfWeek = (day, n) => {
   const currentIndex = daysOfTheWeek.indexOf(day);
-  const newIndex = currentIndex + n;
-  return [currentIndex, newIndex, daysOfTheWeek[newIndex]];
+  const newIndex = getIndex(7, currentIndex, n);
+  return daysOfTheWeek[newIndex];
 };
 
 const afterNDays = (days, n) => {
@@ -23,17 +35,24 @@ const afterNDays = (days, n) => {
 
 // Tests
 
+/*
 console.log(afterNDays(['Monday', 'Tuesday', 'Friday'], 1), [
   'Tuesday',
   'Wednesday',
   'Saturday',
 ]);
+
+
 console.log(afterNDays(['Sunday', 'Sunday', 'Sunday'], 1), [
   'Monday',
   'Monday',
   'Monday',
 ]);
+*/
+
+/*
 console.log(afterNDays(['Thursday', 'Monday'], 4), ['Monday', 'Friday']);
+
 console.log(afterNDays(['Sunday'], 7), ['Sunday']);
 console.log(afterNDays(['Monday', 'Friday', 'Wednesday'], 15), [
   'Tuesday',
@@ -46,3 +65,4 @@ console.log(afterNDays(['Saturday', 'Wednesday', 'Wednesday'], 10), [
   'Saturday',
   'Saturday',
 ]);
+*/
