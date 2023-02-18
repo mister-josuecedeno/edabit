66065b7224b63a0ca33ac1bd705dcc023d128a15
mister-josuecedeno <josuecedeno@yahoo.com>
2023-02-18 08:49:33 -0600

    getStudentsWithNamesAndAvgNote: finish

--- 04 - Hard/getStudentsWithNamesAndAvgNote.js
+++ 04 - Hard/getStudentsWithNamesAndAvgNote.js
@@ -1,7 +1,35 @@
 // https://edabit.com/challenge/aZiwEFEdFvWoBvDWS
 
 const getStudentsWithNamesAndAvgNote = (students) => {
-  return;
+  const result = [];
+
+  for (const student of students) {
+    let sum = student.notes.reduce((acc, cv) => acc + cv, 0);
+    let avg = student.notes.length === 0 ? 0 : sum / student.notes.length;
+    result.push({
+      name: student.name,
+      avgNote: avg,
+    });
+  }
+
+  return result;
 };
 
 // Tests
+const arg = [{ name: 'John', notes: [3, 5, 4] }];
+
+console.log(getStudentsWithNamesAndAvgNote(arg), [
+  { name: 'John', avgNote: 4 },
+]);
+
+const arg2 = [
+  { name: 'Jacek', notes: [] },
+  { name: 'Ewa', notes: [] },
+  { name: 'Zygmunt', notes: [1, 2, 3] },
+];
+
+console.log(getStudentsWithNamesAndAvgNote(arg2), [
+  { name: 'Jacek', avgNote: 0 },
+  { name: 'Ewa', avgNote: 0 },
+  { name: 'Zygmunt', avgNote: 2 },
+]);
--- README.md
+++ README.md
@@ -816,6 +816,7 @@ Additional code examples can be found under my profile in Edabit. This migration
 2023-02-15 - largerThanRight.js\
 2023-02-16 - isEqual.js\
 2023-02-17 - getStudentsWithNamesAndAvgNote.js\
+2023-02-18 - getStudentsWithNamesAndAvgNote.js\
 
 ## Very Hard
 
