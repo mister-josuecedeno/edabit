252cd6252ebc18042ab5ebd7b1bc5d59ebeb0f9b
mister-josuecedeno <josuecedeno@yahoo.com>
2023-03-08 07:11:42 -0600

    isLegitimate: finish

--- 04 - Hard/isLegitimate.js
+++ 04 - Hard/isLegitimate.js
@@ -1,7 +1,21 @@
 // https://edabit.com/challenge/gqkan7642aHfjk5GF
 
-const isLegitimate = (second) => {
-  return;
+const getFence = (arr) => {
+  const fence = [];
+
+  fence.push(arr[0]);
+  fence.push(arr[arr.length - 1]);
+
+  for (let i = 1; i < arr.length - 1; i++) {
+    fence.push(arr[i][0]);
+    fence.push(arr[i][arr[i].length - 1]);
+  }
+
+  return fence.flat();
+};
+
+const isLegitimate = (arr) => {
+  return getFence(arr).every((f) => f === 0);
 };
 
 // Tests
