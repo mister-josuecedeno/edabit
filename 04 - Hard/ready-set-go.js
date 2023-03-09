8ca98a24ae0d970322eac39bf8fa8362f4c8b4b6
mister-josuecedeno <josuecedeno@yahoo.com>
2023-03-09 07:24:37 -0600

    ready-set-go: initial set-up (requires refactor)

--- 04 - Hard/ready-set-go.js
+++ 04 - Hard/ready-set-go.js
@@ -1,26 +1,68 @@
 // https://edabit.com/challenge/2FF7RKw9RLwc3EBY9
-
+// Test and Refactor the results
 class Challenge {
-  // Build class Challenge: constructor + getter
+  constructor(id, level) {
+    this.id = id;
+    this.level = level;
+  }
+
+  points = () => {
+    xp = {
+      VE: 5,
+      EA: 10,
+      ME: 20,
+      HA: 40,
+      VH: 80,
+      EX: 120,
+    };
+
+    return xp(this.level);
+  };
 }
 
 class User {
-  // Build class User: constructor + setter
+  constructor(name, xp, log) {
+    this.name = name;
+    this.xp = xp;
+    this.log = log;
+  }
+
+  newSolvedChallenge = (Challenge) => {
+    ++Challenge.xp;
+    this.log.push(Challenge.id);
+  };
 }
 
 // 1. Declare the class instances (two users and six challenges)
+const user1 = new User('Madam', 0, []);
+const user2 = new User('Steve', 0, []);
+
+const challenge1 = new Challenge(1, 'VE');
+const challenge2 = new Challenge(2, 'EA');
+const challenge3 = new Challenge(3, 'ME');
+const challenge4 = new Challenge(4, 'HA');
+const challenge5 = new Challenge(5, 'VH');
+const challenge6 = new Challenge(6, 'EX');
+
 // 2. Set the challenges solved by the users
+user1.newSolvedChallenge(challenge1);
+user1.newSolvedChallenge(challenge4);
+user1.newSolvedChallenge(challenge6);
+
+user2.newSolvedChallenge(challenge5);
+user2.newSolvedChallenge(challenge3);
+user2.newSolvedChallenge(challenge2);
 
 // Tests
 
 // Test user1
 console.log(user1.name, 'Madam');
 console.log(user1.xp, 165);
-Test.assertSimilar(user1.log, [1, 4, 6]);
+console.log(user1.log, [1, 4, 6]);
 // Test user2
 console.log(user2.name, 'Steve');
 console.log(user2.xp, 110);
-Test.assertSimilar(user2.log, [5, 3, 2]);
+console.log(user2.log, [5, 3, 2]);
 // Test challenges
 console.log(challenge1.id, 1);
 console.log(challenge2.level, 'EA');
