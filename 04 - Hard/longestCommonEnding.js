0f76eed45695ee0e64b90c5a5684f057a1170cf4
mister-josuecedeno <josuecedeno@yahoo.com>
2023-03-26 10:40:33 -0500

    longestCommonEnding: set-up

---
+++ 04 - Hard/longestCommonEnding.js
@@ -0,0 +1,14 @@
+// https://edabit.com/challenge/FGuBhmmvXgkvvTQHc
+
+const longestCommonEnding = (str1, str2) => {
+  return;
+};
+
+// Tests
+
+console.log(longestCommonEnding('pitiful', 'beautiful'), 'tiful');
+console.log(longestCommonEnding('truck', 'trick'), 'ck');
+console.log(longestCommonEnding('vote', 'asymptote'), 'ote');
+console.log(longestCommonEnding('multiplication', 'ration'), 'ation');
+console.log(longestCommonEnding('potent', 'tent'), 'tent');
+console.log(longestCommonEnding('skyscraper', 'carnivore'), '');
