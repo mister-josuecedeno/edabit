328aa3a2474f7edbdf555fee02ba9e940270c2e4
mister-josuecedeno <josuecedeno@yahoo.com>
2023-03-29 06:58:16 -0500

    lcmOfArray: finish

--- 04 - Hard/lcmOfArray.js
+++ 04 - Hard/lcmOfArray.js
@@ -1,8 +1,23 @@
 // https://edabit.com/challenge/qmQ9kRz6ZbkL4hi5F
 
-const lcmOfArray = (second) => {
-  return;
-};
+// Source: https://stackoverflow.com/questions/34953778/calculate-the-lcm-of-two-or-three-numbers-in-javascript
+
+function gcd2(a, b) {
+  // Greatest common divisor of 2 integers
+  if (!b) return b === 0 ? a : NaN;
+  return gcd2(b, a % b);
+}
+
+function lcm2(a, b) {
+  // Least common multiple of 2 integers
+  return (a * b) / gcd2(a, b);
+}
+function lcmOfArray(array) {
+  // Least common multiple of a list of integers
+  var n = 1;
+  for (var i = 0; i < array.length; ++i) n = lcm2(array[i], n);
+  return n;
+}
 
 // Tests
 
