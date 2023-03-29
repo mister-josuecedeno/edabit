e52354c23aaa11d9abf1019efe7490d2e79410f8
mister-josuecedeno <josuecedeno@yahoo.com>
2023-03-29 06:48:39 -0500

    lcmOfArray: set-up

---
+++ 04 - Hard/lcmOfArray.js
@@ -0,0 +1,12 @@
+// https://edabit.com/challenge/qmQ9kRz6ZbkL4hi5F
+
+const lcmOfArray = (second) => {
+  return;
+};
+
+// Tests
+
+console.log(lcmOfArray([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]), 2520);
+console.log(lcmOfArray([13, 6, 17, 18, 19, 20, 37]), 27965340);
+console.log(lcmOfArray([44, 64, 12, 17, 65]), 2333760);
+console.log(lcmOfArray([200, 30, 18, 11, 8, 64, 34]), 2692800);
--- README.md
+++ README.md
@@ -856,6 +856,7 @@ Additional code examples can be found under my profile in Edabit. This migration
 2023-03-26 - longestCommonEnding.js\
 2023-03-27 - persistence.js\
 2023-03-28 - primorial.js\
+2023-03-29 - lcmOfArray.js\
 
 ## Very Hard
 
