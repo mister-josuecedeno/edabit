bf41f3a75dcd994077c64a6428645789c817cf62
mister-josuecedeno <josuecedeno@yahoo.com>
2023-04-09 09:29:26 -0500

    fullKeyName: set-up

---
+++ 04 - Hard/fullKeyName.js
@@ -0,0 +1,65 @@
+// https://edabit.com/challenge/mTT8M47D6EuEdTXLv
+
+const fullKeyName = (second) => {
+  return;
+};
+
+// Tests
+
+console.log(fullKeyName('Prelude in C'), 'Prelude in C major');
+console.log(fullKeyName('Fugue in c'), 'Fugue in C minor');
+console.log(
+  fullKeyName('Toccata and Fugue in d'),
+  'Toccata and Fugue in D minor'
+);
+console.log(fullKeyName('a Fugue in c'), 'a Fugue in C minor');
+console.log(fullKeyName('Sonata in D'), 'Sonata in D major');
+console.log(fullKeyName('Opera in eb'), 'Opera in Eb minor');
+console.log(fullKeyName('Toccata in ab'), 'Toccata in Ab minor');
+console.log(fullKeyName('Toccata in f'), 'Toccata in F minor');
+console.log(fullKeyName('Symphony in C#'), 'Symphony in C# major');
+console.log(fullKeyName('Mass in c'), 'Mass in C minor');
+console.log(fullKeyName('Toccata in c'), 'Toccata in C minor');
+console.log(fullKeyName('Prelude in Ab'), 'Prelude in Ab major');
+console.log(fullKeyName('Concerto in f#'), 'Concerto in F# minor');
+console.log(fullKeyName('Symphony in eb'), 'Symphony in Eb minor');
+console.log(fullKeyName('Sonata in a'), 'Sonata in A minor');
+console.log(fullKeyName('Sonata in e'), 'Sonata in E minor');
+console.log(fullKeyName('Concerto in c#'), 'Concerto in C# minor');
+console.log(fullKeyName('Opera in f'), 'Opera in F minor');
+console.log(fullKeyName('Mass in c'), 'Mass in C minor');
+console.log(fullKeyName('Symphony in D'), 'Symphony in D major');
+console.log(fullKeyName('Fugue in d'), 'Fugue in D minor');
+console.log(fullKeyName('Fugue in eb'), 'Fugue in Eb minor');
+console.log(fullKeyName('Fugue in F'), 'Fugue in F major');
+console.log(fullKeyName('Mass in c#'), 'Mass in C# minor');
+console.log(fullKeyName('Requiem in C'), 'Requiem in C major');
+console.log(fullKeyName('Prelude in Eb'), 'Prelude in Eb major');
+console.log(fullKeyName('Prelude in Bb'), 'Prelude in Bb major');
+console.log(fullKeyName('Fugue in g'), 'Fugue in G minor');
+console.log(fullKeyName('Requiem in c#'), 'Requiem in C# minor');
+console.log(fullKeyName('Opera in f'), 'Opera in F minor');
+console.log(fullKeyName('Prelude in G'), 'Prelude in G major');
+console.log(fullKeyName('Symphony in Eb'), 'Symphony in Eb major');
+console.log(fullKeyName('Symphony in eb'), 'Symphony in Eb minor');
+console.log(fullKeyName('Sonata in e'), 'Sonata in E minor');
+console.log(fullKeyName('Fugue in g'), 'Fugue in G minor');
+console.log(fullKeyName('Sonata in A'), 'Sonata in A major');
+console.log(fullKeyName('Requiem in b'), 'Requiem in B minor');
+console.log(fullKeyName('Sonata in eb'), 'Sonata in Eb minor');
+console.log(fullKeyName('Prelude in c#'), 'Prelude in C# minor');
+console.log(fullKeyName('Fugue in F'), 'Fugue in F major');
+console.log(fullKeyName('Concerto in ab'), 'Concerto in Ab minor');
+console.log(fullKeyName('Toccata in F#'), 'Toccata in F# major');
+console.log(fullKeyName('Symphony in Bb'), 'Symphony in Bb major');
+console.log(fullKeyName('Waltz in f#'), 'Waltz in F# minor');
+console.log(fullKeyName('Prelude in Eb'), 'Prelude in Eb major');
+console.log(fullKeyName('Requiem in b'), 'Requiem in B minor');
+console.log(fullKeyName('Concerto in Bb'), 'Concerto in Bb major');
+console.log(fullKeyName('Fugue in G'), 'Fugue in G major');
+console.log(fullKeyName('Mass in eb'), 'Mass in Eb minor');
+console.log(fullKeyName('Symphony in Ab'), 'Symphony in Ab major');
+console.log(fullKeyName('Symphony in f'), 'Symphony in F minor');
+console.log(fullKeyName('Fugue in f#'), 'Fugue in F# minor');
+console.log(fullKeyName('Prelude in D'), 'Prelude in D major');
+console.log(fullKeyName('Concerto in bb'), 'Concerto in Bb minor');
--- README.md
+++ README.md
@@ -867,6 +867,7 @@ Additional code examples can be found under my profile in Edabit. This migration
 2023-04-06 - differenceMaxMin.cs\
 2023-04-07 - letsMeet.js\
 2023-04-08 - FirstLast.cs\
+2023-04-09 - fullKeyName.js\
 
 ## Very Hard
 
