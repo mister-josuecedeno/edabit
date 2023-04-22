6bf5312e58ae200574ff505a52a700a301f0d75a
mister-josuecedeno <josuecedeno@yahoo.com>
2023-04-22 09:09:52 -0500

    goldbachConjecture: refactor

--- 04 - Hard/goldbachConjecture.js
+++ 04 - Hard/goldbachConjecture.js
@@ -1,20 +1,29 @@
 // https://edabit.com/challenge/CtzrFDd3wpDtu9TCg
 
-const goldbachConjecture = (second) => {
-  return;
+// Still needs to be refactored
+const goldbachConjecture = (n) => {
+  // if odd and greater than 2 return an empty array
+  if (n % 2 != 0 && n > 2) return [];
+
+  // if even and greater than 2
+  if (n % 2 === 0 && n > 2) {
+    return [-1];
+  }
+
+  return [];
 };
 
 // Tests
 
-Test.assertSimilar(goldbachConjecture(1), []);
-Test.assertSimilar(goldbachConjecture(4), [2, 2]);
-Test.assertSimilar(goldbachConjecture(7), []);
-Test.assertSimilar(goldbachConjecture(8), [3, 5]);
-Test.assertSimilar(goldbachConjecture(10), [3, 7]);
-Test.assertSimilar(goldbachConjecture(24), [5, 19]);
-Test.assertSimilar(goldbachConjecture(100), [3, 97]);
-Test.assertSimilar(goldbachConjecture(1234), [3, 1231]);
-Test.assertSimilar(goldbachConjecture(1400), [19, 1381]);
-Test.assertSimilar(goldbachConjecture(1566), [7, 1559]);
-Test.assertSimilar(goldbachConjecture(3444), [11, 3433]);
+console.log(goldbachConjecture(1), []);
+console.log(goldbachConjecture(4), [2, 2]);
+console.log(goldbachConjecture(7), []);
+console.log(goldbachConjecture(8), [3, 5]);
+console.log(goldbachConjecture(10), [3, 7]);
+console.log(goldbachConjecture(24), [5, 19]);
+console.log(goldbachConjecture(100), [3, 97]);
+console.log(goldbachConjecture(1234), [3, 1231]);
+console.log(goldbachConjecture(1400), [19, 1381]);
+console.log(goldbachConjecture(1566), [7, 1559]);
+console.log(goldbachConjecture(3444), [11, 3433]);
 // Mubashir
