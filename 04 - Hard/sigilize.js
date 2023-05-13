65a3c058adef22ca0070127119c90426c7ddb38c
mister-josuecedeno <josuecedeno@yahoo.com>
2023-05-13 13:03:44 -0500

    sigilize: set-up

---
+++ 04 - Hard/sigilize.js
@@ -0,0 +1,17 @@
+// https://edabit.com/challenge/kqup45hRWqWQ7LqDm
+
+const sigilize = (desire) => {
+  return;
+};
+
+// Tests
+
+console.log(sigilize('I HAVE WONDERFUL FRIENDS WHO LOVE ME'), 'FRNDSWHLVM');
+console.log(sigilize('My business is financially successful'), 'MBNYCSFL');
+console.log(sigilize('I have a job I enjoy and it pays well'), 'HVBJNDTPYSWL');
+console.log(
+  sigilize('I heard the song closing time playing on the radio'),
+  'CSMPLYGNTHRD'
+);
+console.log(sigilize('i am healthy'), 'MLTHY');
+console.log(sigilize('I FOUND MY SOULMATE'), 'FNDYSLMT');
