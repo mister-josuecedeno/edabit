0c878dd88beaeb0bb35a0af207103b688ebe6ebf
mister-josuecedeno <josuecedeno@yahoo.com>
2023-06-17 07:57:41 -0500

    sockPairs: set-up

---
+++ 04 - Hard/sockPairs.js
@@ -0,0 +1,17 @@
+// https://edabit.com/challenge/AkSERJZ9pTEZfFMNE
+
+const sockPairs = (second) => {
+  return;
+};
+
+// Tests
+
+console.log(sockPairs('AA'), 1);
+console.log(sockPairs('ABABC'), 2);
+console.log(sockPairs('CABBACCC'), 4);
+console.log(sockPairs('AACDE'), 1);
+console.log(sockPairs('ACDBE'), 0);
+console.log(sockPairs(''), 0);
+console.log(sockPairs('ABABAB'), 2);
+console.log(sockPairs('AAAAA'), 2);
+console.log(sockPairs('AAACCBB'), 3);
--- README.md
+++ README.md
@@ -937,6 +937,7 @@ Additional code examples can be found under my profile in Edabit. This migration
 2023-06-14 - countWords.cs\
 2023-06-15 - digitSort.js\
 2023-06-16 - Days.cs\
+2023-06-17 - sockPairs.js\
 
 ## Very Hard
 
