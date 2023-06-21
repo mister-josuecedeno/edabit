fa7b9862ff17e12ea9b2caacc9e9a71807d86a82
mister-josuecedeno <josuecedeno@yahoo.com>
2023-06-21 07:48:18 -0500

    boundSort: refactor

--- 04 - Hard/boundSort.js
+++ 04 - Hard/boundSort.js
@@ -1,14 +1,11 @@
 // https://edabit.com/challenge/riBaZGcbdEwBKmqzr
 
-// Refactor
-const checkArrays = (arr1, arr2) => {
-  return arr1.every((num1) => arr2.every((num2) => num1 < num2));
-};
-
+// refactor - read the instructions again
 const boundSort = (arr, bounds) => {
-  const arr1 = arr.slice(...bounds);
-  const arr2 = arr.slice(bounds[1] + 1);
-  return checkArrays(arr1, arr2);
+  if (bounds[1] === arr.length - 1) return true;
+  const arr1 = arr.slice(...bounds).sort();
+  const arr2 = arr.slice(bounds[1] + 1).sort();
+  return arr1.slice(-1)[0] <= arr2[0];
 };
 
 // Tests
