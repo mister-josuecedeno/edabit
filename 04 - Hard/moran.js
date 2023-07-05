55ff06876c6091f9675ceadd07efd31531270afa
mister-josuecedeno <josuecedeno@yahoo.com>
2023-07-05 16:24:20 -0500

    moran: finish

--- 04 - Hard/moran.js
+++ 04 - Hard/moran.js
@@ -1,7 +1,27 @@
 // https://edabit.com/challenge/tLBEdP8Kp2WSJo7bo
 
+const isPrime = (n) => {
+  let count = 0;
+
+  for (let i = 1; i <= n && count < 3; i++) {
+    if (n % i === 0) count++;
+  }
+
+  return count === 2;
+};
+
+const sumOfDigits = (n) => {
+  return [...`${n}`].reduce((acc, cv) => acc + +cv, 0);
+};
+
 const moran = (n) => {
-  return;
+  const div = n / sumOfDigits(n);
+  const harshad = Number.isInteger(div);
+  const moran = isPrime(div);
+
+  if (moran) return 'M';
+  if (harshad) return 'H';
+  return 'Neither';
 };
 
 // Tests
