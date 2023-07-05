8051788b6869d13cc45c50018bf4f6b70d6f16cf
mister-josuecedeno <josuecedeno@yahoo.com>
2023-07-05 16:09:55 -0500

    moran: set-up

---
+++ 04 - Hard/moran.js
@@ -0,0 +1,15 @@
+// https://edabit.com/challenge/tLBEdP8Kp2WSJo7bo
+
+const moran = (n) => {
+  return;
+};
+
+// Tests
+
+console.log(moran(132), 'H');
+console.log(moran(133), 'M');
+console.log(moran(134), 'Neither');
+console.log(moran(3033), 'M');
+console.log(moran(3030), 'H');
+console.log(moran(491423), 'Neither');
+console.log(moran(20937), 'M');
