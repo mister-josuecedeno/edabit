08e93f89f06aca586b3cee1d89253652a44e328e
Josue Cedeno <47830532+mister-josuecedeno@users.noreply.github.com>
2023-07-10 09:25:37 -0500

    digitalDivision: set-up

---
+++ 04 - Hard/digitalDivision.js
@@ -0,0 +1,22 @@
+// https://edabit.com/challenge/ZSiHm257pcdv9QiXa
+
+const digitalDivision = () => {
+  return;
+}
+
+// Tests
+
+console.log(digitalDivision(21), 1, "Example #1")
+console.log(digitalDivision(128), 2, "Example #2")
+console.log(digitalDivision(100), 2, "Example #3")
+console.log(digitalDivision(12), "Perfect", "Example #4")
+console.log(digitalDivision(31), "Indivisible", "Example #5")
+console.log(digitalDivision(111), "Perfect")
+console.log(digitalDivision(40), 2)
+console.log(digitalDivision(35), "Indivisible")
+console.log(digitalDivision(666), 2)
+console.log(digitalDivision(735), "Perfect")
+console.log(digitalDivision(1890), 1)
+console.log(digitalDivision(444268), "Indivisible")
+console.log(digitalDivision(123456789), "Indivisible")
+console.log(digitalDivision(1), "Perfect")
