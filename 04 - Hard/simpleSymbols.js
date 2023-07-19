d54458114ab64d309e273472ba695acc45246f27
mister-josuecedeno <josuecedeno@yahoo.com>
2023-07-19 11:51:04 -0500

    simpleSymbols: set-up

---
+++ 04 - Hard/simpleSymbols.js
@@ -0,0 +1,12 @@
+// edabit.com/challenge/rsjKiutZuYaBYeaXt
+
+const simpleSymbols = (str) => {
+  return;
+};
+
+// Tests
+console.log(simpleSymbols('======2+++4+u===+i+'), false);
+console.log(simpleSymbols('+u+====3+mmmmm===m++'), false);
+console.log(simpleSymbols('==+p+++++++++====8+z++++'), true);
+console.log(simpleSymbols('+d+=3=+s+'), true);
+console.log(simpleSymbols('f++d+'), false);
