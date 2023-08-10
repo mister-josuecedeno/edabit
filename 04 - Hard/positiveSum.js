454359382ec32239ac977ca5f80bda2be1eeadce
mister-josuecedeno <josuecedeno@yahoo.com>
2023-08-10 07:54:11 -0500

    positiveSum: set-up

---
+++ 04 - Hard/positiveSum.js
@@ -0,0 +1,16 @@
+// https://edabit.com/challenge/xjv7kk6Q7tDhyZTtZ
+
+const positiveSum = (chars) => {
+  return;
+};
+
+// Tests
+
+console.log(positiveSum('-12#-33 13%14&-11'), 27);
+console.log(positiveSum('22 13%14&-11-22 13 12 0'), 74);
+console.log(positiveSum('3%4&-1 13 a 20 -2 b c'), 40);
+console.log(positiveSum('2 3%-144&-1123 -2122'), 5);
+console.log(positiveSum('-123 -8 1'), 1);
+console.log(positiveSum('-122 2'), 2);
+console.log(positiveSum('-121 0 3'), 3);
+console.log(positiveSum('0 12 12 2'), 26);
