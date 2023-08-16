23a935c4c086dd3c610819ad9d42e3ff6578ecd1
mister-josuecedeno <josuecedeno@yahoo.com>
2023-08-16 08:01:34 -0500

    battleOutcome: refactor

--- 04 - Hard/battleOutcome.js
+++ 04 - Hard/battleOutcome.js
@@ -1,14 +1,21 @@
 // https://edabit.com/challenge/nG3NSjoK4BTi9jxBS
 
 const battleOutcome = (num) => {
+  let result = '';
+  const str = `${num}`;
+
+  for (let i = 0; i < str.length; i + 2) {
+    console.log(str[i]);
+  }
+
   return;
 };
 
 // Tests
 
-Test.assertEquals(battleOutcome(32531), 351);
-Test.assertEquals(battleOutcome(111), 1);
-Test.assertEquals(battleOutcome(5289), 59);
-Test.assertEquals(battleOutcome(76811), 781);
-Test.assertEquals(battleOutcome(3245196), 3596);
-Test.assertEquals(battleOutcome(93552129), 929);
+console.log(battleOutcome(32531), 351);
+console.log(battleOutcome(111), 1);
+console.log(battleOutcome(5289), 59);
+console.log(battleOutcome(76811), 781);
+console.log(battleOutcome(3245196), 3596);
+console.log(battleOutcome(93552129), 929);
--- README.md
+++ README.md
@@ -997,6 +997,7 @@ Additional code examples can be found under my profile in Edabit. This migration
 2023-08-13 - wrapAround.js\
 2023-08-14 - Stutter.cs\
 2023-08-15 - battleOutcome.js\
+2023-08-16 - battleOutcome.js\
 
 ## Very Hard
 
