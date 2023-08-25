0e5b07a7ef47ef8853b888efa5dfda0a7586809f
mister-josuecedeno <josuecedeno@yahoo.com>
2023-08-25 07:17:53 -0500

    primeCount: set-up

---
+++ 04 - Hard/primeCount.js
@@ -0,0 +1,16 @@
+// https://edabit.com/challenge/kqeXu4S4cyfzNExk3
+
+const primeCount = (second) => {
+  return;
+};
+
+// Tests
+
+console.log(primeCount(1, 10), 4);
+console.log(primeCount(1, 100), 25);
+console.log(primeCount(1, 1000), 168);
+console.log(primeCount(1, 10000), 1229);
+console.log(primeCount(1, 100000), 9592);
+console.log(primeCount(2090, 2098), 0);
+console.log(primeCount(548, 556), 0);
+console.log(primeCount(3297, 4297), 128);
--- README.md
+++ README.md
@@ -1006,6 +1006,7 @@ Additional code examples can be found under my profile in Edabit. This migration
 2023-08-22 - mySub.js\
 2023-08-23 - mySub.js\
 2023-08-24 - DivisibleByB.cs\
+2023-08-25 - primeCount.js\
 
 ## Very Hard
 
