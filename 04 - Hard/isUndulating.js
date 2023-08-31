ad290648b50d6d779c21b2533f14a1772e0e8950
mister-josuecedeno <josuecedeno@yahoo.com>
2023-08-31 07:15:35 -0500

    isUndulating: set-up

---
+++ 04 - Hard/isUndulating.js
@@ -0,0 +1,18 @@
+// https://edabit.com/challenge/sysGYNhR6ZhKBFt9D
+
+const isUndulating = (second) => {
+  return;
+};
+
+// Tests
+
+console.log(isUndulating(121), true, 'Example #1');
+console.log(isUndulating(373737), true, 'Example #2');
+console.log(isUndulating(12), false, 'Example #3');
+console.log(isUndulating(12122), false, 'Example #4');
+console.log(isUndulating(85856), false, 'Example #5');
+console.log(isUndulating(949494), true);
+console.log(isUndulating(494), true);
+console.log(isUndulating(363738), false);
+console.log(isUndulating(3663), false);
+console.log(isUndulating(81818181), true);
