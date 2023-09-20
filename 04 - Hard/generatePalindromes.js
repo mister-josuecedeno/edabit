05a19190df734ec2d3af16d946d2205109bb1167
mister-josuecedeno <josuecedeno@yahoo.com>
2023-09-20 07:46:42 -0500

    generatePalindromes: finish

--- 04 - Hard/generatePalindromes.js
+++ 04 - Hard/generatePalindromes.js
@@ -1,7 +1,28 @@
 // https://edabit.com/challenge/Ggh7GYrLKvWHqyk59
 
+const isPalindrome = (str, i = 0, isP) => {
+  const mid = Math.ceil(str.length / 2);
+  if (i >= mid || isP === false) return isP;
+
+  let left = str[i];
+  let right = str[str.length - (i + 1)];
+  isP = left === right;
+
+  return isPalindrome(str, i + 1, isP);
+};
+
 const generatePalindromes = (limit) => {
-  return;
+  const nums = [];
+
+  while (nums.length < 15) {
+    if (isPalindrome(`${limit}`)) {
+      nums.push(limit);
+    }
+
+    limit--;
+  }
+
+  return nums.sort((a, b) => a - b);
 };
 
 // Tests
--- README.md
+++ README.md
@@ -1032,6 +1032,7 @@ Additional code examples can be found under my profile in Edabit. This migration
 2023-09-17 - productEqualTarget.js\
 2023-09-18 - productEqualTarget.js\
 2023-09-19 - generatePalindromes.js\
+2023-09-20 - generatePalindromes.js\
 
 ## Very Hard
 
