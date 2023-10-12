68328e5e5936d2d0313067f5d19b10cd22b0e787
mister-josuecedeno <josuecedeno@yahoo.com>
2023-10-12 07:10:48 -0500

    lychrel: set-up

---
+++ 04 - Hard/lychrel.js
@@ -0,0 +1,18 @@
+// https://edabit.com/challenge/d8gGWEAJuxaPrGoiT
+
+const lychrel = (n) => {
+  return;
+};
+
+// Tests
+
+console.log(lychrel(33), 0);
+console.log(lychrel(65), 1);
+console.log(lychrel(348), 3);
+console.log(lychrel(196), true);
+console.log(lychrel(89), 24);
+console.log(lychrel(7582), 4);
+console.log(lychrel(1945), true);
+console.log(lychrel(3673), true);
+console.log(lychrel(9485367), 2);
+console.log(lychrel(695), 3);
--- README.md
+++ README.md
@@ -1054,6 +1054,7 @@ Additional code examples can be found under my profile in Edabit. This migration
 2023-10-09 - ReverseCapitalize.cs\
 2023-10-10 - ReverseCapitalize.cs\
 2023-10-11 - ReverseCapitalize.cs\
+2023-10-12 - lychrel.js\
 
 ## Very Hard
 
