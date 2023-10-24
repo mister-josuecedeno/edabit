74a371f82cc20e3b0b222225e1a4f0e3d0091534
mister-josuecedeno <josuecedeno@yahoo.com>
2023-10-24 08:04:12 -0500

    checkIfDannyIsHere: set-up

---
+++ 04 - Hard/checkIfDannyIsHere.js
@@ -0,0 +1,18 @@
+// https://edabit.com/challenge/mxmZby7FHAW47yTAX
+
+const checkIfDannyIsHere = (str) => {
+  return;
+};
+
+// Tests
+
+console.log(checkIfDannyIsHere('is there dan here ?'), false);
+console.log(checkIfDannyIsHere('and here dan ny ?'), false);
+console.log(checkIfDannyIsHere('I found you d4nny '), true);
+console.log(checkIfDannyIsHere('danny is here'), true);
+console.log(
+  checkIfDannyIsHere(
+    'best still sitting |\\|ot cut main computer mouse film cut.'
+  ),
+  false
+);
