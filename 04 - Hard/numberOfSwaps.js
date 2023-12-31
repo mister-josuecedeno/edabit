1d557aa6ebd4d7ee99edd24e38d96c826b74d6b0
mister-josuecedeno <josuecedeno@yahoo.com>
2023-12-31 11:39:17 -0600

    numberOfSwaps: refactor

--- 04 - Hard/numberOfSwaps.js
+++ 04 - Hard/numberOfSwaps.js
@@ -1,7 +1,35 @@
 // https://edabit.com/challenge/6ALbTxgu8BZaa6YYN
 
+// Refactor
+const equalsCheck = (a, b) => {
+  return JSON.stringify(a) === JSON.stringify(b);
+};
+
 const numberOfSwaps = (arr) => {
-  return;
+  const sorted = [...arr].sort();
+  let i = 0;
+  let swaps = 0;
+
+  while (!equalsCheck(arr, sorted)) {
+    let current = arr[i];
+    let next = arr[i + 1];
+
+    if (current > next) {
+      let temp = next;
+      arr[i + 1] = current;
+      arr[i] = temp;
+    }
+
+    if (i === arr.length - 1) {
+      i = 0;
+      swaps++;
+    }
+
+    i++;
+    //break;
+  }
+
+  return swaps;
 };
 
 // Tests
