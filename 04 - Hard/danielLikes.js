00ccd2dd84c6a74b47def3ab5c5bc8e1c639c4d0
mister-josuecedeno <josuecedeno@yahoo.com>
2024-01-17 07:12:41 -0600

    danielLikes: finished

--- 04 - Hard/danielLikes.js
+++ 04 - Hard/danielLikes.js
@@ -1,11 +1,17 @@
 // https://edabit.com/challenge/nbBxCfxPNy4ovFoqQ
 
-const danielLikes = (girl) => {
-  return [...girl].map((l) => l.charCodeAt(0));
-};
+// Not my answer
+function danielLikes(str) {
+  let result = 0;
+  for (let i = 0; i < str.length; i++) {
+    result += str.charCodeAt(i);
+  }
+  return (result + '')[0] == 5;
+}
 
 // Tests
-console.log(danielLikes('Daniel'), false);
-console.log(danielLikes('Renata'), false);
-console.log(danielLikes('Charli'), true);
+console.log(danielLikes('Daniel'), true);
 console.log(danielLikes('Holly'), true);
+console.log(danielLikes('Renata'), false);
+console.log(danielLikes('Katelyn'), false);
+console.log(danielLikes('Charli'), true);
