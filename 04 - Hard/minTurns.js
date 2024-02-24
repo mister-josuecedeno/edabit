fae9438bb2301fcfc3ae54d0e8173fb87ea1e761
mister-josuecedeno <josuecedeno@yahoo.com>
2024-02-24 09:55:42 -0600

    minTurns: finished

--- 04 - Hard/minTurns.js
+++ 04 - Hard/minTurns.js
@@ -1,7 +1,46 @@
 // https://edabit.com/challenge/sWo3y2WgtGDDtRtrB
 
+const forwards = (current, target) => {
+  const max = 9;
+  let turns = 0;
+
+  while (current != target) {
+    current++;
+    turns++;
+    if (current > max) {
+      current = 0;
+    }
+  }
+
+  return turns;
+};
+
+const backwards = (current, target) => {
+  const min = 0;
+  let turns = 0;
+
+  while (current != target) {
+    current--;
+    turns++;
+    if (current < min) {
+      current = 9;
+    }
+  }
+
+  return turns;
+};
+
 const minTurns = (current, target) => {
-  return;
+  const currentNumbers = [...current];
+  const targetNumbers = [...target];
+  let turns = 0;
+
+  for (let i = 0; i < current.length; i++) {
+    let f = forwards(currentNumbers[i], targetNumbers[i]);
+    let b = backwards(currentNumbers[i], targetNumbers[i]);
+    turns += f < b ? f : b;
+  }
+  return turns;
 };
 
 // Tests
