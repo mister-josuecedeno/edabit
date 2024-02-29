8b8cf748b422027077f49ac29eec503def6b33c7
mister-josuecedeno <josuecedeno@yahoo.com>
2024-02-29 07:38:42 -0600

    zipper: in progress

--- 04 - Hard/zipper.js
+++ 04 - Hard/zipper.js
@@ -1,9 +1,19 @@
 // https://edabit.com/challenge/KJDD2N2putddr5vp3
 
 const zipper = (arr1, arr2) => {
-  return;
-};
+  if (arr1.join('') == arr2.join('')) return true;
+  let num1, num2;
 
+  while (num1 === num2) {
+    num1 = arr1.pop();
+    num2 = arr2.pop();
+  }
+
+  let noChange = arr1.length > 0 && arr1.length == arr2.length;
+  if (noChange) return false;
+
+  return [arr1.length, arr2.length];
+};
 // Tests
 
 console.log(
@@ -14,3 +24,9 @@ console.log(zipper([5, 4, 3, 2, 6], [6, 4, 3, 2, 6]), [0, 0]);
 console.log(zipper([5, 4, 3, 2, 7], [6, 4, 3, 2, 6]), false);
 console.log(zipper([1, 5, 4, 3], [9, 8, 7, 5, 4, 3]), [0, 2]);
 console.log(zipper([7, 8, 9, 9, 9], [8, 8, 9, 9, 9, 9, 9]), [1, 3]);
+console.log(zipper([7, 8, 5, 1, 5], [6, 6, 5, 1, 5]), [1, 1]);
+console.log(zipper([6, 6, 5, 1, 5], [6, 6, 5, 1, 5]), true);
+console.log(
+  zipper([1, 1, 2, 6, 6, 5, 1, 5], [7, 8, 9, 4, 4, 6, 6, 5, 1, 5]),
+  [2, 4]
+);
--- README.md
+++ README.md
@@ -1194,6 +1194,7 @@ Additional code examples can be found under my profile in Edabit. This migration
 2024-02-26 - LettersOnly.cs\
 2024-02-27 - LettersOnly.cs\
 2024-02-28 - zipper.js\
+2024-02-29 - zipper.js\
 
 ## Very Hard
 
