2ab94748aa5b9d808e1c74551b50c0d3ae3e1383
mister-josuecedeno <josuecedeno@yahoo.com>
2024-03-29 07:04:21 -0500

    nDifferences: set-up

---
+++ 04 - Hard/nDifferences.js
@@ -0,0 +1,14 @@
+// https://edabit.com/challenge/zYhCuzcXZn9PKG6ry
+
+const nDifferences = (arr) => {
+  return;
+};
+
+// Tests
+
+console.log(nDifferences([5, 1, 9, 3, 4, 0]), -80);
+console.log(nDifferences([8, 9, 2, 5, 4, 3, 3, 1, 6]), -94);
+console.log(nDifferences([5, 1, 9, 6, 1, 7, 3, 4]), 118);
+console.log(nDifferences([1, 1, 1, 1]), 0);
+console.log(nDifferences([5, 9, 7, 3]), 4);
+console.log(nDifferences([1, 5, 3, 9, 7]), -30);
--- README.md
+++ README.md
@@ -1223,6 +1223,7 @@ Additional code examples can be found under my profile in Edabit. This migration
 2024-03-26 - GreaterThanOne.cs\
 2024-03-27 - maxHam.js\
 2024-03-28 - GetMiddle.cs\
+2024-03-29 - nDifferences.js\
 
 ## Very Hard
 
