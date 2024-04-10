4a1b4fb45ca233b84359a7907e41ff409f8030c9
mister-josuecedeno <josuecedeno@yahoo.com>
2024-04-10 06:52:54 -0500

    closingInSum: set-up

---
+++ 04 - Hard/closingInSum.js
@@ -0,0 +1,17 @@
+// https://edabit.com/challenge/vrHDieytY657gYTcT
+
+const closingInSum = (num) => {
+  return;
+};
+
+// Tests
+
+console.log(closingInSum(10710356439481), 205);
+console.log(closingInSum(403672023435605), 248);
+console.log(closingInSum(121), 13);
+console.log(closingInSum(1039), 22);
+console.log(closingInSum(22225555), 100);
+console.log(closingInSum(2520), 72);
+console.log(closingInSum(5332824166496569), 331);
+console.log(closingInSum(72314137318116), 237);
+console.log(closingInSum(44013947776), 160);
