fa0faed84c917c28d75586d7f53b7390c43a0657
mister-josuecedeno <josuecedeno@yahoo.com>
2024-05-11 16:58:00 -0500

    checkerBoard: set-up

---
+++ 04 - Hard/checkerBoard.js
@@ -0,0 +1,39 @@
+// https://edabit.com/challenge/TaH2g7iSJBHgqJemi
+
+const checkerBoard = (n, el1, el2) => {
+  return;
+};
+
+// Tests
+
+console.log(checkerBoard(3, 'A', 'B'), [
+  ['A', 'B', 'A'],
+  ['B', 'A', 'B'],
+  ['A', 'B', 'A'],
+]);
+
+console.log(checkerBoard(2, 7, 6), [
+  [7, 6],
+  [6, 7],
+]);
+
+console.log(checkerBoard(3, 1, 0), [
+  [1, 0, 1],
+  [0, 1, 0],
+  [1, 0, 1],
+]);
+
+console.log(checkerBoard(3, 0, 1), [
+  [0, 1, 0],
+  [1, 0, 1],
+  [0, 1, 0],
+]);
+
+console.log(checkerBoard(4, 'c', 'd'), [
+  ['c', 'd', 'c', 'd'],
+  ['d', 'c', 'd', 'c'],
+  ['c', 'd', 'c', 'd'],
+  ['d', 'c', 'd', 'c'],
+]);
+
+console.log(checkerBoard(4, 'c', 'c'), 'invalid');
