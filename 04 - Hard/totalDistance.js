bf0b0faec7e49acbfa13f63a87958b0705584a87
mister-josuecedeno <josuecedeno@yahoo.com>
2024-05-23 07:55:43 -0500

    totalDistance: set-up

---
+++ 04 - Hard/totalDistance.js
@@ -0,0 +1,15 @@
+// https://edabit.com/challenge/cFhKd3LzSD73vNKb8
+
+const totalDistance = (fuel, fuelUsage, passengers, airCon) => {
+  return;
+};
+
+// Tests
+
+console.log(totalDistance(30.0, 3.0, 0, false), 1000.0);
+console.log(totalDistance(50.0, 7.0, 3, true), 564.7);
+console.log(totalDistance(36.1, 8.6, 3, true), 331.8);
+console.log(totalDistance(71.9, 12.1, 1, false), 565.9);
+console.log(totalDistance(11.0, 11.0, 0, false), 100.0);
+console.log(totalDistance(55.5, 5.5, 5, false), 807.3);
+console.log(totalDistance(300.0, 25.0, 11, true), 703.8);
--- README.md
+++ README.md
@@ -1278,6 +1278,7 @@ Additional code examples can be found under my profile in Edabit. This migration
 2024-05-20 - IsPalindrome.cs\
 2024-05-21 - IsPalindrome.cs\
 2024-05-22 - totalDistance.js\
+2024-05-23 - totalDistance.js\
 
 ## Very Hard
 
