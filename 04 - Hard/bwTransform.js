5d88f34732fe8ac9c7bb671f952c805066d5cdd2
mister-josuecedeno <josuecedeno@yahoo.com>
2024-06-05 07:47:49 -0500

    bwTransform: set-up

---
+++ 04 - Hard/bwTransform.js
@@ -0,0 +1,15 @@
+// https://edabit.com/challenge/Sdj4jE7tHuoiyNKFv
+
+const bwTransform = (text) => {
+  return;
+};
+
+// Tests
+
+console.log(bwTransform('banana$'), 'annb$aa');
+console.log(bwTransform('mississippi$'), 'ipssm$pissii');
+console.log(bwTransform('abaaba$'), 'abba$aa');
+console.log(
+  bwTransform('acccgtttgtttcaatagatccatcaa$'),
+  'aacc$tacgttctaccatcaatatttgg'
+);
--- README.md
+++ README.md
@@ -1291,6 +1291,7 @@ Additional code examples can be found under my profile in Edabit. This migration
 2024-06-02 - totalDistance.js\
 2024-06-03 - IsIsogram.cs\
 2024-06-04 - IsIsogram.cs\
+2024-06-05 - bwTransform.js\
 
 ## Very Hard
 
