b22786094fda205250db6a8bb5737bbd050dfd34
mister-josuecedeno <josuecedeno@yahoo.com>
2024-06-10 07:41:08 -0500

    isPolydivisible: set-up

---
+++ 04 - Hard/isPolydivisible.js
@@ -0,0 +1,13 @@
+// https://edabit.com/challenge/krzm6Y2riBDYJJa2R
+
+const isPolydivisible = (n) => {
+  return;
+};
+
+// Tests
+
+console.log(isPolydivisible(1232), true);
+console.log(isPolydivisible(123220), false);
+console.log(isPolydivisible(0), true);
+console.log(isPolydivisible(1), true);
+console.log(isPolydivisible(141), true);
