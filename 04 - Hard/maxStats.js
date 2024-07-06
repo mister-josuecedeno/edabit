438d0efcd4b96bd64041f2815bf93cbcf5fcef41
Josue Cedeno <josuecedeno@yahoo.com>
2024-07-06 09:48:24 -0500

    maxStats: finished

--- 04 - Hard/maxStats.js
+++ 04 - Hard/maxStats.js
@@ -1,107 +1,25 @@
 // https://edabit.com/challenge/5N2g3vGhjhy2QcviD
 
-// Resources
+// Not my answer
 
-const characters = {
-  Knight: {
-    attack: 120,
-    defense: 140,
-    speed: 6,
-  },
-  Warrior: {
-    attack: 180,
-    defense: 71,
-    speed: 8,
-  },
-  Fairy: {
-    attack: 71,
-    defense: 100,
-    speed: 16,
-  },
-  Robot: {
-    attack: 160,
-    defense: 120,
-    speed: 11,
-  },
-  Giant: {
-    attack: 160,
-    defense: 200,
-    speed: 4,
-  },
+const chars = {
+  Knight: [120, 140, 6],
+  Warrior: [180, 71, 8],
+  Fairy: [71, 100, 16],
+  Robot: [160, 120, 11],
+  Giant: [160, 200, 4],
 };
 
-const weapons = {
-  SimpleSword: {
-    attack: 10,
-    gold: 20,
-  },
-  Katana: {
-    attack: 20,
-    gold: 40,
-  },
-  SharpenedSword: {
-    attack: 30,
-    gold: 60,
-  },
-  GreatSword: {
-    attack: 40,
-    gold: 80,
-  },
-  ForgottenSword: {
-    attack: 50,
-    gold: 100,
-  },
-};
+const weapons = [50, 40, 30, 20, 10, 0].map((v) => [v, v * 2]);
+const armor = [100, 80, 60, 40, 20, 0].map((v) => [v, v * 1.5]);
+const boots = [15, 12, 9, 6, 3, 0].map((v) => [v, 8 * v]);
 
-const armor = {
-  BronzeArmor: {
-    defense: 20,
-    gold: 30,
-  },
-  IronArmor: {
-    defense: 40,
-    gold: 60,
-  },
-  SteelArmor: {
-    defense: 60,
-    gold: 90,
-  },
-  ObsidianArmor: {
-    defense: 80,
-    gold: 120,
-  },
-  DragonhideArmor: {
-    defense: 100,
-    gold: 150,
-  },
-};
-
-const boots = {
-  SimpleBoots: {
-    speed: 3,
-    gold: 24,
-  },
-  LeatherBoots: {
-    speed: 6,
-    gold: 48,
-  },
-  StrongBoots: {
-    speed: 9,
-    gold: 72,
-  },
-  CompoundBoots: {
-    speed: 12,
-    gold: 96,
-  },
-  SoftBoots: {
-    speed: 15,
-    gold: 120,
-  },
-};
-
-const maxStats = (second) => {
-  return;
-};
+function maxStats(character, gold) {
+  const bestEquip = (eqList) => eqList.find((v) => v[1] <= gold)[0];
+  let r = chars[character];
+  [weapons, armor, boots].forEach((v, i) => (r[i] += bestEquip(v)));
+  return r;
+}
 
 // Tests
 
