d313882f57c8a692c7eb03d1b2de31b7d8b2627d
mister-josuecedeno <josuecedeno@yahoo.com>
2024-07-17 19:07:53 -0500

    getCoinBalances: refactor

--- 04 - Hard/getCoinBalances.js
+++ 04 - Hard/getCoinBalances.js
@@ -1,7 +1,25 @@
 // https://edabit.com/challenge/PuGi7j9rm5nHBX8Ec
 
-const getCoinBalances = (second) => {
-  return;
+const getCoins = (player, opponent) => {
+  let coins = 0;
+  if ((player = 'share')) coins -= 1;
+  if ((opponent = 'share')) coins += 3;
+  return coins;
+};
+
+const getCoinBalances = (arr1, arr2) => {
+  let p1Coins = 3;
+  let p2Coins = 3;
+
+  for (let i = 0; i < arr1.length; i++) {
+    let p1Action = arr1[i];
+    let p2Action = arr2[i];
+
+    p1Coins += getCoins(p1Action, p2Action);
+    p2Coins += getCoins(p2Action, p1Action);
+  }
+
+  return [p1Coins, p2Coins];
 };
 
 // Tests
