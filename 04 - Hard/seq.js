10fdd2b5a474c9068649e46657e320152eaa57e3
Josue Cedeno <josuecedeno@yahoo.com>
2024-09-20 07:08:51 -0500

    seq: set-up

---
+++ 04 - Hard/seq.js
@@ -0,0 +1,13 @@
+// https://edabit.com/challenge/SaP7mAdsQCrN4ShCi
+
+const seq = (second) => { return; }
+
+// Tests
+
+console.log(seq(1), 2)
+console.log(seq(2), 6)
+console.log(seq(0), 1, "If the formula is right, 0 can be an input.")
+console.log(seq(18), 478)
+console.log(seq(27), 1081)
+console.log(seq(6), 52)
+console.log(seq(99), 14653)
