8564661366baa5afbdaabafe05b96e351c50442e
Josue Cedeno <josuecedeno@yahoo.com>
2024-10-30 07:05:13 -0500

    canBuild: set-up

---
+++ 04 - Hard/canBuild2.js
@@ -0,0 +1,12 @@
+// https://edabit.com/challenge/aKx7FLtttZqopDo5e
+
+const canBuild = (s1, s2) => { return; }
+
+// Tests
+
+console.log(canBuild("got 2 go", "go go go 2 today"), true)
+console.log(canBuild("for an angel", "angel forest nymph awaken"), true)
+console.log(canBuild("sit on top", "its a moo point"), true)
+console.log(canBuild("solar to oven", "love desolate rose thorn"), true)
+console.log(canBuild("gate im in", "magnetizing"), true)
+console.log(canBuild("moreso", "mount rushmore"), true)
