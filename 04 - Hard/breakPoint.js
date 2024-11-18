3fcf4ffa52a1e1a271c6311a773e5f2ea9658626
Josue Cedeno <josuecedeno@yahoo.com>
2024-11-18 07:09:38 -0600

    breakPoint: set-up

---
+++ 04 - Hard/breakPoint.js
@@ -0,0 +1,14 @@
+// https://edabit.com/challenge/eoFDKdjN9tFXWcEQi
+
+const breakPoint = (num) => { return; }
+
+// Tests
+
+console.log(breakPoint(159780), true)
+console.log(breakPoint(112), true)
+console.log(breakPoint(10), false)
+console.log(breakPoint(1034), true)
+console.log(breakPoint(343), false)
+console.log(breakPoint(1119444), true)
+console.log(breakPoint(6666), true)
+console.log(breakPoint(9777771), false)
