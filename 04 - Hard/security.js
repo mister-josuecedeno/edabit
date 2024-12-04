9d6c310fe427827a77531cfb0d5c864510351cb8
Josue Cedeno <josuecedeno@yahoo.com>
2024-12-04 07:09:00 -0600

    security: in progress

--- 04 - Hard/security.js
+++ 04 - Hard/security.js
@@ -1,6 +1,9 @@
 // https://edabit.com/challenge/SimxWM2Tz9jvXqLM3
 
-const security = (str) => { return; }
+const security = (str) => {
+  const regex = /[GT$]/g;
+  return str.match(regex);
+}
 
 // Tests
 
@@ -9,3 +12,8 @@ console.log(security("xGxx$xxGxxxTTT"), "Safe")
 console.log(security("TxGxx$xxx$xxxGxxT"), "Safe")
 console.log(security("GxxxTxxGxxTxx$xx$xxTxxG"), "ALARM!")
 console.log(security("xxGTxx$xx$xxxxxxG"), "ALARM!")
+console.log(security("xxTxxxxxxxx$xGxxxxxxT"), "ALARM!")
+console.log(security("xx$xxGxxxx$xxxxxxxxxxT"), "ALARM!")
+console.log(security("xxxTxxxxT"), "Safe")
+console.log(security("xxGxxxGGG"), "Safe")
+console.log(security("x$xx$x$x$xx"), "Safe")
