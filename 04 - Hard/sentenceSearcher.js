6f88f4af732c224aab2eedc0c228b0fe2d7a7494
Josue Cedeno <josuecedeno@yahoo.com>
2024-12-23 07:30:55 -0600

    sentenceSearcher: set-up

---
+++ 04 - Hard/sentenceSearcher.js
@@ -0,0 +1,12 @@
+// https://edabit.com/challenge/cLpDSS2HwAq86wgyv
+
+const sentenceSearcher = (str, word) => { return; }
+
+// Tests
+
+console.log(sentenceSearcher(txt, "cat"), "I have a cat.")
+console.log(sentenceSearcher(txt, "MAT"), "I have a mat.")
+console.log(sentenceSearcher(txt, "Have"), "I have a cat.")
+console.log(sentenceSearcher(txt, "swell"), "Things are going swell.")
+console.log(sentenceSearcher(txt, "things"), "Things are going swell.")
+console.log(sentenceSearcher(txt, "flat"), "")
