380291d42205414205288cba873c2e14f07d0b0c
Josue Cedeno <josuecedeno@yahoo.com>
2024-12-29 21:54:03 -0600

    equalize: set-up

---
+++ 04 - Hard/equalize.js
@@ -0,0 +1,11 @@
+// https://edabit.com/challenge/6xfeFywjKqinKsmwd
+
+const equalize = (arr, c) => {
+  return;
+}
+
+// Tests
+
+console.log(equalize([1, 2, 2, 1, 2, 1, 2, 2, 2, 1, 1, 1], 2), 4)
+console.log(equalize([5, 2, 3, 5, 2, 2, 3, 5, 1, 2, 5, 1, 2, 5, 3], 7), 2)
+console.log(equalize([1, 2, 2, 1, 1, 1, 2, 2, 2, 1, 1, 1], 9), 1)
