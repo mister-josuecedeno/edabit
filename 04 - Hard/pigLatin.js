b45bde1665bdc73c1b3c45b947c0b345369baa3e
Josue Cedeno <josuecedeno@yahoo.com>
2025-01-01 11:37:42 -0600

    pigLatin: set-up

---
+++ 04 - Hard/pigLatin.js
@@ -0,0 +1,11 @@
+// https://edabit.com/challenge/4rEcJErhoTRbNRnKH
+
+const pigLatin = (second) => { return; }
+
+// Tests
+
+console.log(pigLatin("Cats are great pets."), "Atscay areway reatgay etspay.")
+console.log(pigLatin("Tom got a small piece of pie."), "Omtay otgay away mallsay iecepay ofway iepay.")
+console.log(pigLatin("He told us a very exciting tale."), "Ehay oldtay usway away eryvay excitingway aletay.")
+console.log(pigLatin("A diamond is not enough."), "Away iamondday isway otnay enoughway.")
+console.log(pigLatin("Hurry!"), "Urryhay!")
