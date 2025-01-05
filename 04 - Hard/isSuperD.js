6b76d5cd007618169e25aa434b448d1599d28557
Josue Cedeno <josuecedeno@yahoo.com>
2025-01-05 17:50:32 -0600

    isSuperD: set-up

---
+++ 04 - Hard/isSuperD.js
@@ -0,0 +1,13 @@
+// https://edabit.com/challenge/fw8qrxpz2iGWh5QcE
+
+const isSuperD = (n) => { return; }
+
+// Tests
+
+console.log(isSuperD(19), "Super-2 Number", "Example #1");
+console.log(isSuperD(753), "Super-3 Number", "Example #2");
+console.log(isSuperD(1168), "Super-4 Number", "Example #3");
+console.log(isSuperD(24), "Normal Number", "Example #4");
+console.log(isSuperD(20379), "Super-5 Number");
+console.log(isSuperD(185423), "Super-8 Number");
+console.log(isSuperD(1170), "Normal Number");
