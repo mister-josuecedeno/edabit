089c1a14828117f4eddb39d10dbd9e98d8ce3e2c
Josue Cedeno <josuecedeno@yahoo.com>
2025-01-09 07:50:05 -0600

    alphabetIndex: finished

--- 04 - Hard/alphabetIndex.js
+++ 04 - Hard/alphabetIndex.js
@@ -1,10 +1,22 @@
 // https://edabit.com/challenge/zujgM52Xr4MogBjgH
 
-const alphabetIndex = (second) => { return; }
+const alphabetIndex = (alphabet, str) => {
+
+  let max = 0;
+
+  for (let i = 0; i < str.length; i++) {
+    let index = alphabet.indexOf(str[i].toLowerCase());
+    if (index > max) {
+      max = index;
+    }
+  }
+
+  return max + 1 + alphabet[max];
+}
 
 // Tests
+const alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
 
-var alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
 console.log(alphabetIndex(alphabet, "Oscar"), "19s")
 console.log(alphabetIndex(alphabet, "Lucas"), "21u")
 console.log(alphabetIndex(alphabet, "Marko"), "18r")
