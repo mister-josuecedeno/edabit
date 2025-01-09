47b9433e30372c33f14b0207a04fa54c820fbc69
Josue Cedeno <josuecedeno@yahoo.com>
2025-01-09 07:26:12 -0600

    alphabetIndex: set-up

---
+++ 04 - Hard/alphabetIndex.js
@@ -0,0 +1,16 @@
+// https://edabit.com/challenge/zujgM52Xr4MogBjgH
+
+const alphabetIndex = (second) => { return; }
+
+// Tests
+
+var alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+console.log(alphabetIndex(alphabet, "Oscar"), "19s")
+console.log(alphabetIndex(alphabet, "Lucas"), "21u")
+console.log(alphabetIndex(alphabet, "Marko"), "18r")
+console.log(alphabetIndex(alphabet, "Esley"), "25y")
+console.log(alphabetIndex(alphabet, "Rogei"), "18r")
+console.log(alphabetIndex(alphabet, "Nicolas"), "19s")
+console.log(alphabetIndex(alphabet, "Flavio"), "22v")
+console.log(alphabetIndex(alphabet, "Andrey"), "25y")
+console.log(alphabetIndex(alphabet, "Hello"), "15o")
