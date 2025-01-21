303177b113ed237d1095e4bc80e248784e83e373
Josue Cedeno <josuecedeno@yahoo.com>
2025-01-21 07:05:12 -0600

    correctSigns: set-up

---
+++ 04 - Hard/correctSigns.js
@@ -0,0 +1,13 @@
+// https://edabit.com/challenge/Y2H2aDQhfeE9bHg6m
+
+const correctSigns = (second) => { return; }
+
+// Tests
+
+console.log(correctSigns("3 < 7 < 11"), true)
+console.log(correctSigns("13 > 44 > 33 > 1"), false)
+console.log(correctSigns("1 < 2 < 6 < 9 > 3"), true)
+console.log(correctSigns("4 > 3 > 2 > 1"), true)
+console.log(correctSigns("5 < 7 > 1"), true)
+console.log(correctSigns("5 > 7 > 1"), false)
+console.log(correctSigns("9 < 9"), false)
