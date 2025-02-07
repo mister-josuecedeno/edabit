2e05e36777e30985f8b709e897bc2698a0f6b451
Josue Cedeno <josuecedeno@yahoo.com>
2025-02-07 07:09:41 -0600

    averageASCII: refactor

--- 04 - Hard/averageASCII.js
+++ 04 - Hard/averageASCII.js
@@ -1,12 +1,24 @@
 // https://edabit.com/challenge/qfFpvDpeKb9f68x64
 
 const averageASCII = (str) => {
-  // Get sum of the ASCII vlaues for each word
-  // Get the average ASCII value of the string
-  // If the ASCII value is greater than the average, convert the word to uppercase  
+  // Get sum of the ASCII values for each word
+  const sums = str.split(' ').map(word => {
+    return word.split('').reduce((acc, char) => acc + char.charCodeAt(0), 0);
+  });
+
+  // Get average of the ASCII values for each word
+  const avg = sums.reduce((acc, sum) => acc + sum, 0) / sums.length;
+
+  // If the ASCII value is greater than the average, convert the word to uppercase 
+  const words = str.split(' ').map((word, index) => {
+    if (sums[index] > avg) {
+      return word.toUpperCase();
+    }
+    return word;
+  });
 
   // Join the array with a space
-  return;
+  return words.join(' ');
 }
 
 // Tests
@@ -17,3 +29,4 @@ console.log(averageASCII("What you egg"), "WHAT you egg")
 console.log(averageASCII("Made by Harith Shah"), "Made by HARITH Shah")
 console.log(averageASCII("Boom"), "Boom")
 console.log(averageASCII("The most addictive way to learn"), "The most ADDICTIVE way to LEARN")
+console.log(averageASCII("Tell me the time"), "The most ADDICTIVE way to LEARN")
