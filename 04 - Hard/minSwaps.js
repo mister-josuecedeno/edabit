de352db8f5feb5f7dbdd7a5955a5b98e7f8f01da
Josue Cedeno <josuecedeno@yahoo.com>
2025-02-17 07:06:58 -0600

    minSwaps: set-up

---
+++ 04 - Hard/minSwaps.js
@@ -0,0 +1,13 @@
+// https://edabit.com/challenge/Wx5EF6o38tAY2stmh
+
+const minSwaps = (str) => { return; }
+
+// Tests
+
+console.log(minSwaps("010010111"), 4)
+console.log(minSwaps("010101010"), 0)
+console.log(minSwaps("101010100"), 1)
+console.log(minSwaps("101010000"), 2)
+console.log(minSwaps("101000000"), 3)
+console.log(minSwaps("10001"), 1)
+console.log(minSwaps("10010"), 2)
