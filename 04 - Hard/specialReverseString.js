13e03c0019762f9067bc2d15bc8eca046f6ee24e
Josue Cedeno <josuecedeno@yahoo.com>
2025-03-10 06:54:04 -0500

    specialReverseString: set-up

---
+++ 04 - Hard/specialReverseString.js
@@ -0,0 +1,15 @@
+// https://edabit.com/challenge/AcL4HiKbC9cYSHmYt
+
+const specialReverseString = (second) => {
+  return;
+}
+
+// Tests
+
+console.log(specialReverseString('Edabit'), 'Tibade')
+console.log(specialReverseString('UPPER lower'), 'REWOL reppu')
+console.log(specialReverseString('1 23 456'), '6 54 321')
+console.log(specialReverseString('Hello World!'), '!dlro Wolleh')
+console.log(specialReverseString("Where's your dog Daisy?"), "?ysiadg odru oys 'erehw")
+console.log(specialReverseString('addition(3, 2) ➞ 5'), '5➞)2,3(noit id d a')
+console.log(specialReverseString("It's known that CSS means Cascading Style Sheets"), "Stee hsely tsgn IDA csacs Naemsscta Htnwo Nks'ti")
