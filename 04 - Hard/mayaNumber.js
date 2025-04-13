1b5d84a91b4645e9b377b752ffb2e1fd952812da
Josue Cedeno <josuecedeno@yahoo.com>
2025-04-13 09:54:45 -0500

    mayaNumber

---
+++ 04 - Hard/mayaNumber.js
@@ -0,0 +1,14 @@
+// https://edabit.com/challenge/n6ts9wQi6Dcb6bmP7
+
+const mayaNumber = (n) => { return; }
+
+// Tests
+
+console.log(mayaNumber(39), ["o", "oooo———"], "Example #1")
+console.log(mayaNumber(815), ["oo", "@", "———"], "Example #2")
+console.log(mayaNumber(16125), ["oo", "@", "o—", "—"], "Example #3")
+console.log(mayaNumber(0), ["@"])
+console.log(mayaNumber(1985), ["oooo", "oooo———", "—"])
+console.log(mayaNumber(86420), ["——", "o———", "o", "@"])
+console.log(mayaNumber(13579), ["o", "ooo——", "ooo———", "oooo———"])
+console.log(mayaNumber(666), ["o", "ooo——", "o—"])
