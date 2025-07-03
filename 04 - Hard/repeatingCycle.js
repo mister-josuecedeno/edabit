1fbd47cd826f94299fa1329bcf467fdf9b77aa12
Josue Cedeno <josuecedeno@yahoo.com>
2025-07-03 07:46:19 -0500

    repeatingCycle

---
+++ 04 - Hard/repeatingCycle.js
@@ -0,0 +1,31 @@
+// https://edabit.com/challenge/bYK2XtQJRE88b2yQH
+
+const repeatingCycle = (n) => {
+  return;
+}
+
+// Tests
+
+console.log(repeatingCycle(33), 2)
+console.log(repeatingCycle(18118), -1)
+console.log(repeatingCycle(69), 22)
+console.log(repeatingCycle(197), 98)
+console.log(repeatingCycle(65), -1)
+console.log(repeatingCycle(97), 96)
+console.log(repeatingCycle(19), 18)
+console.log(repeatingCycle(111), 3)
+console.log(repeatingCycle(53), 13)
+console.log(repeatingCycle(59), 58)
+console.log(repeatingCycle(93), 15)
+console.log(repeatingCycle(51), 16)
+console.log(repeatingCycle(159), 13)
+console.log(repeatingCycle(183), 60)
+console.log(repeatingCycle(197), 98)
+console.log(repeatingCycle(167), 166)
+console.log(repeatingCycle(94), -1)
+console.log(repeatingCycle(133), 18)
+console.log(repeatingCycle(218713), 9744)
+console.log(repeatingCycle(38127), 6230)
+console.log(repeatingCycle(431541), 726)
+console.log(repeatingCycle(221193), 3510)
+console.log(repeatingCycle(1234567), 34020)
